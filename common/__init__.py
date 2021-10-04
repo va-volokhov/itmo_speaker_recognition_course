774c59db5fd4a50010181d81891bb92b9908d1c1
Volokhov Vladimir <volokhov@speechpro.com>
2021-10-04 15:21:16 +0300

    CORRECT __init__.py in common

--- common/__init__.py
+++ common/__init__.py
@@ -1,4 +1,4 @@
 from .dataprep import download_dataset, concatenate, extract_dataset, part_extract, download_protocol, split_musan
-from .DatasetLoader import test_dataset_loader, loadWAV, AugmentWAV, train_dataset_sampler
+from .DatasetLoader import test_dataset_loader, loadWAV, AugmentWAV
 from .perf import ecdf, get_eer
 from .scoring import extract_features, compute_scores
