1abf930446925e2e3ed5fe436fbbca6221be5edf
novoselov <novoselov@speechpro.com>
2022-02-11 17:51:23 +0300

    lab2 exercises ready

--- lab2/exercises.py
+++ lab2/exercises.py
@@ -2,15 +2,17 @@
 
 
 # Import of modules
+from typing import Tuple, List, Any, Tuple, Dict, Optional, AnyStr, Path
 import numpy as np
 import scipy.signal
 
 from skimage.morphology import opening, closing
 
 
-def load_vad_markup(path_to_rttm, signal, fs):
-    # Function to read rttm files and generate VAD's markup in samples
+def load_vad_markup(path_to_rttm: Path, signal: np.ndarray, fs: int) -> np.ndarray:
     """
+    Function to read rttm files and generate VAD's markup in samples
+
     :param path_to_rttm: path to rttm markup file
     :param signal: raw input signal
     :param fs: sampling frequency
@@ -34,9 +36,10 @@ def load_vad_markup(path_to_rttm, signal, fs):
     return vad_markup
 
 
-def framing(signal, window=320, shift=160):
-    # Function to create frames from signal
+def framing(signal: np.ndarray, window: int=320, shift: int=160) -> np.ndarray:
     """
+    Function creates frames from signal
+
     :param signal: raw input signal
     :param window: size of sliding window in samples
     :param shift: size of the sliding step in samples
@@ -50,52 +53,56 @@ def framing(signal, window=320, shift=160):
     return frames
 
 
-def frame_energy(frames):
-    # Function to compute frame energies
+def frame_energy(frames: np.ndarray) -> np.ndarray:
     """
+    Function to compute frame energies
+
     :param frames: matrix of frames of the signal [n_frames x win_size]
     :return: E: energies vector [n_frames x 1]
     """
-    E = frames.sum(axis=1)
+    e = frames.sum(axis=1)
     
-    return E
+    return e
 
 
-def norm_energy(E):
-    # Function to normalize energy by mean energy and energy standard deviation
+def norm_energy(e: np.ndarray) -> np.ndarray:
     """
-    :param E: signal sliding frames energies vector [n_frames x 1]
-    :return: E_norm: normalised energies vector [n_frames x 1]
+    Function normalizes energy by mean energy and energy standard deviation
+
+    :param e: signal sliding frames energies vector [n_frames x 1]
+    :return: e_norm: normalised energies vector [n_frames x 1]
     """
-    E_norm = np.copy(E)
-    E_norm -= E_norm.mean()
-    E_norm /= (E_norm.std() + 1e-10)
+    e_norm = np.copy(e)
+    e_norm -= e_norm.mean()
+    e_norm /= (e_norm.std() + 1e-10)
     
-    return E_norm
+    return e_norm
 
 
-def gmm_train(E, gauss_pdf, n_realignment):
-    # Function to train parameters of gaussian mixture model using EM-algorithm
+def gmm_train(e: np.ndarray, gauss_pdf: Any, n_realignment: int) \
+        -> Tuple[Union[np.ndarray, float], Union[np.ndarray, float], Union[np.ndarray, float]]:
     """
-    :param E: time series of input data [n_frames x 1]
+    Function to train parameters of gaussian mixture model using EM-algorithm
+
+    :param e: time series of input data [n_frames x 1]
     :param gauss_pdf: gaussian probability density function
     :param n_realignment: the number of EM iterations
     :return: w - weights parameters of gmm,
              m - means parameters of gmm,
              sigma - sigmas parameters of gmm
     """
-    
-    # Initialization gaussian mixture models
-    w     = np.array([ 0.33, 0.33, 0.33])
-    m     = np.array([-1.00, 0.00, 1.00])
-    sigma = np.array([ 1.00, 1.00, 1.00])
 
-    g = np.zeros([len(E), len(w)])
+    # Initialization gaussian mixture models
+    w = np.array([0.33, 0.33, 0.33])
+    m = np.array([-1.00, 0.00, 1.00])
+    sigma = np.array([1.00, 1.00, 1.00])
+
+    g = np.zeros([len(e), len(w)])
     for n in range(n_realignment):
 
         # E-step
         for j in range(len(m)):
-            g[:, j] = gauss_pdf(E, m[j], sigma[j])
+            g[:, j] = gauss_pdf(e, m[j], sigma[j])
 
         g_norm = np.zeros(g.shape[0])
         for j in range(len(m)):
@@ -105,49 +112,63 @@ def gmm_train(E, gauss_pdf, n_realignment):
             g[:, j] = w[j]*g[:, j]/(g_norm + 1e-10)
 
         # M-step
-        w = g.sum(axis=0)/len(E)
+        w = g.sum(axis=0)/len(e)
 
-        M = np.zeros([len(E), len(w)])
+        m = np.zeros([len(e), len(w)])
         for j in range(len(m)):
-            M[:, j] = g[:, j]*E
+            m[:, j] = g[:, j] * e
 
-        m = M.sum(axis=0)/len(E)/(w + 1e-10)
+        m = m.sum(axis=0) / len(e) / (w + 1e-10)
 
-        S = np.zeros([len(E), len(w)])
+        s = np.zeros([len(e), len(w)])
         for j in range(len(m)):
-            S[:, j] = g[:, j]*(E - m[j])**2
+            s[:, j] = g[:, j] * (e - m[j]) ** 2
 
-        sigma = S.sum(axis=0)/len(E)/(w + 1e-10)
+        sigma = s.sum(axis=0) / len(e) / (w + 1e-10)
         sigma = np.sqrt(sigma)
         
     return w, m, sigma
 
 
-def eval_frame_post_prob(E, gauss_pdf, w, m, sigma):
-    # Function to estimate a posterior probability that frame isn't speech
+def eval_frame_post_prob(e: np.ndarray,
+                         gauss_pdf: Any,
+                         w: Union[np.ndarray, float],
+                         m: Union[np.ndarray, float],
+                         sigma: Union[np.ndarray, float]) -> np.ndarray:
     """
-    :param E: time series of input data [n_frames x 1]
+    Function to estimate a posterior probability that frame isn't speech
+
+    :param e: time series of input data [n_frames x 1]
     :param gauss_pdf: gaussian probability density function
     :param w: weights parameters of gmm
     :param m: means parameters of gmm
     :param sigma: sigmas parameters of gmm
-    :return: posterior probability time vector
+    :return: posterior probability time vector gamma_p
     """
-    g = np.zeros([len(E), len(w)])
+    g = np.zeros([len(e), len(w)])
     
     for j in range(len(m)):
-        g[:, j] = gauss_pdf(E, m[j], sigma[j])
+        g[:, j] = gauss_pdf(e, m[j], sigma[j])
 
     g_norm = np.zeros(g.shape[0])
     for j in range(len(m)):
         g_norm = g_norm + w[j]*g[:, j]
-            
-    return w[0]*g[:, 0]/(g_norm + 1e-10)
+
+    gamma_p = w[0]*g[:, 0]/(g_norm + 1e-10)
+
+    return gamma_p
 
 
-def energy_gmm_vad(signal, window, shift, gauss_pdf, n_realignment, vad_thr, mask_size_morph_filt):
-    # Function to compute markup energy voice activity detector based of gaussian mixtures model
+def energy_gmm_vad(signal: np.ndarray,
+                   window: int,
+                   shift: int,
+                   gauss_pdf: Any,
+                   n_realignment: int,
+                   vad_thr: float,
+                   mask_size_morph_filt: int) -> np.ndarray:
     """
+    Function to compute markup energy voice activity detector based of gaussian mixtures model
+
     :param signal: raw input signal
     :param window: size of sliding window in samples
     :param shift: size of the sliding step in samples
@@ -165,35 +186,39 @@ def energy_gmm_vad(signal, window, shift, gauss_pdf, n_realignment, vad_thr, mas
     frames = framing(squared_signal, window=window, shift=shift)
     
     # Sum frames to get energy
-    E = frame_energy(frames)
+    e = frame_energy(frames)
     
     # Normalize the energy
-    E_norm = norm_energy(E)
+    e_norm = norm_energy(e)
     
     # Train parameters of gaussian mixture models
-    w, m, sigma = gmm_train(E_norm, gauss_pdf, n_realignment=10)
+    w, m, sigma = gmm_train(e_norm, gauss_pdf, n_realignment=n_realignment)
     
     # Estimate a posterior probability that frame isn't speech
-    g0 = eval_frame_post_prob(E_norm, gauss_pdf, w, m, sigma)
+    g0 = eval_frame_post_prob(e_norm, gauss_pdf, w, m, sigma)
     
     # Compute real VAD's markup
-    vad_frame_markup_real = (g0 < vad_thr).astype('float32')  # frame VAD's markup
-
-    vad_markup_real = np.zeros(len(signal)).astype('float32') # sample VAD's markup
+    vad_frame_markup_real = (g0 < vad_thr).astype('float32')
+    # Compute sample VAD's markup
+    vad_markup_real = np.zeros(len(signal)).astype('float32')
     for idx in range(len(vad_frame_markup_real)):
         vad_markup_real[idx*shift:shift+idx*shift] = vad_frame_markup_real[idx]
 
     vad_markup_real[len(vad_frame_markup_real)*shift - len(signal):] = vad_frame_markup_real[-1]
     
     # Morphology Filters
-    vad_markup_real = closing(vad_markup_real, np.ones(mask_size_morph_filt)) # close filter
-    vad_markup_real = opening(vad_markup_real, np.ones(mask_size_morph_filt)) # open filter
+    # close filter
+    vad_markup_real = closing(vad_markup_real, np.ones(mask_size_morph_filt))
+    # open filter
+    vad_markup_real = opening(vad_markup_real, np.ones(mask_size_morph_filt))
     
     return vad_markup_real
 
-def reverb(signal, impulse_response):
-    # Function to create reverberation effect
+
+def reverb(signal: np.ndarray, impulse_response: np.ndarray) -> np.ndarray:
     """
+    Function to create reverberation effect
+
     :param signal: raw input signal
     :param impulse_response: impulse response for augmentation
     :return: signal_reverb: reverberated signal
@@ -205,9 +230,11 @@ def reverb(signal, impulse_response):
     
     return signal_reverb
 
-def awgn(signal, sigma_noise):
-    # Function to add white gaussian noise to signal
+
+def awgn(signal: np.ndarray, sigma_noise: float) -> np.ndarray:
     """
+    Function to add white gaussian noise to signal
+
     :param signal: raw input signal
     :param sigma_noise: sigma of noise to add
     :return: signal_noise: noised signal
--- lab2/exercises_blank.py
+++ lab2/exercises_blank.py
@@ -2,72 +2,117 @@
 
 
 # Import of modules
+from typing import Tuple, List, Any, Tuple, Dict, Optional, AnyStr, Path
 import numpy as np
 import scipy.signal
 
 from skimage.morphology import opening, closing
 
 
-def load_vad_markup(path_to_rttm, signal, fs):
-    # Function to read rttm files and generate VAD's markup in samples
-    
+def load_vad_markup(path_to_rttm: Path, signal: np.ndarray, fs: int) -> np.ndarray:
+    """
+    Function to read rttm files and generate VAD's markup in samples
+
+    :param path_to_rttm: path to rttm markup file
+    :param signal: raw input signal
+    :param fs: sampling frequency
+    :return: vad_markup: VAD's markup in samples
+
+    TODO: Here you should read rttm files and generate VAD's markup in samples
+    """
+
     vad_markup = np.zeros(len(signal)).astype('float32')
-        
+
     ###########################################################
     # Here is your code
-    
+
     ###########################################################
-    
+
     return vad_markup
 
-def framing(signal, window=320, shift=160):
-    # Function to create frames from signal
-    
-    shape   = (int((signal.shape[0] - window)/shift + 1), window)
-    frames  = np.zeros().astype('float32')
+
+def framing(signal: np.ndarray, window: int = 320, shift: int = 160) -> np.ndarray:
+    """
+    Function creates frames from signal
+
+    :param signal: raw input signal
+    :param window: size of sliding window in samples
+    :param shift: size of the sliding step in samples
+    :return: frames [n_frames x win_size]
+
+    TODO: Here you need to prepare framed signal
+    """
+    shape = (int((signal.shape[0] - window) / shift + 1), window)
+    frames = np.zeros().astype('float32')
 
     ###########################################################
     # Here is your code
-    
+
     ###########################################################
-    
     return frames
 
-def frame_energy(frames):
-    # Function to compute frame energies
-    
-    E = np.zeros(frames.shape[0]).astype('float32')
+
+def frame_energy(frames: np.ndarray) -> np.ndarray:
+    """
+    Function to compute frame energies
+
+    :param frames: matrix of frames of the signal [n_frames x win_size]
+    :return: E: energies vector [n_frames x 1]
+
+    TODO: Here you need to compute time energies from framed signal
+    """
+    e = np.zeros(frames.shape[0]).astype('float32')
 
     ###########################################################
     # Here is your code
-    
-    ###########################################################
-    
-    return E
 
-def norm_energy(E):
-    # Function to normalize energy by mean energy and energy standard deviation
-    
-    E_norm = np.zeros(len(E)).astype('float32')
+    ###########################################################
+
+    return e
+
+
+def norm_energy(e: np.ndarray) -> np.ndarray:
+    """
+    Function normalizes energy by mean energy and energy standard deviation
+
+    :param e: signal sliding frames energies vector [n_frames x 1]
+    :return: e_norm: normalised energies vector [n_frames x 1]
+
+    TODO: Here you need to normalize time energies
+    """
+    e_norm = np.zeros(len(E)).astype('float32')
 
     ###########################################################
     # Here is your code
-    
-    ###########################################################
-    
-    return E_norm
 
-def gmm_train(E, gauss_pdf, n_realignment):
-    # Function to train parameters of gaussian mixture model
-    
+    ###########################################################
+
+    return e_norm
+
+
+def gmm_train(e: np.ndarray, gauss_pdf: Any, n_realignment: int) \
+        -> Tuple[Union[np.ndarray, float], Union[np.ndarray, float], Union[np.ndarray, float]]:
+    """
+    Function to train parameters of gaussian mixture model using EM-algorithm
+
+    :param e: time series of input data [n_frames x 1]
+    :param gauss_pdf: gaussian probability density function
+    :param n_realignment: the number of EM iterations
+    :return: w - weights parameters of gmm,
+             m - means parameters of gmm,
+             sigma - sigmas parameters of gmm
+
+    TODO: Here you need to train GMM model using EM iterations
+    """
+
     # Initialization gaussian mixture models
-    w     = np.array([ 0.33, 0.33, 0.33])
-    m     = np.array([-1.00, 0.00, 1.00])
-    sigma = np.array([ 1.00, 1.00, 1.00])
+    w = np.array([0.33, 0.33, 0.33])
+    m = np.array([-1.00, 0.00, 1.00])
+    sigma = np.array([1.00, 1.00, 1.00])
 
-    g = np.zeros([len(E), len(w)])
+    g = np.zeros([len(e), len(w)])
     for n in range(n_realignment):
-
+        print('{} iteration'%(n))
         # E-step
         ###########################################################
         # Here is your code
@@ -79,77 +124,132 @@ def gmm_train(E, gauss_pdf, n_realignment):
         # Here is your code
 
         ###########################################################
-        
+
     return w, m, sigma
 
-def eval_frame_post_prob(E, gauss_pdf, w, m, sigma):
-    # Function to estimate a posterior probability that frame isn't speech
 
-    g0 = np.zeros(len(E))
+def eval_frame_post_prob(e: np.ndarray,
+                         gauss_pdf: Any,
+                         w: Union[np.ndarray, float],
+                         m: Union[np.ndarray, float],
+                         sigma: Union[np.ndarray, float]) -> np.ndarray:
+    """
+    Function to estimate a posterior probability that frame isn't speech
+
+    :param e: time series of input data [n_frames x 1]
+    :param gauss_pdf: gaussian probability density function
+    :param w: weights parameters of gmm
+    :param m: means parameters of gmm
+    :param sigma: sigmas parameters of gmm
+    :return: posterior probability time vector gamma_p
+
+    TODO: Here you need to compute a posterior probability vector that each frame isn't speech
+    """
+    gamma_p = np.zeros(len(e))
 
     ###########################################################
     # Here is your code
 
     ###########################################################
-            
-    return g0
 
-def energy_gmm_vad(signal, window, shift, gauss_pdf, n_realignment, vad_thr, mask_size_morph_filt):
-    # Function to compute markup energy voice activity detector based of gaussian mixtures model
-    
+    return gamma_p
+
+
+def energy_gmm_vad(signal: np.ndarray,
+                   window: int,
+                   shift: int,
+                   gauss_pdf: Any,
+                   n_realignment: int,
+                   vad_thr: float,
+                   mask_size_morph_filt: int) -> np.ndarray:
+    """
+    Function to compute markup energy voice activity detector based of gaussian mixtures model
+
+    :param signal: raw input signal
+    :param window: size of sliding window in samples
+    :param shift: size of the sliding step in samples
+    :param gauss_pdf: gaussian probability density function
+    :param n_realignment: number of EM iterations
+    :param vad_thr: posterior threshold
+    :param mask_size_morph_filt: the size of mask for Morphology Filter
+    :return: vad_markup_real: VAD's markup in samples
+    """
+
     # Squared signal
-    squared_signal = signal**2
-    
+    squared_signal = signal ** 2
+
     # Frame signal with overlap
     frames = framing(squared_signal, window=window, shift=shift)
-    
+
     # Sum frames to get energy
-    E = frame_energy(frames)
-    
+    e = frame_energy(frames)
+
     # Normalize the energy
-    E_norm = norm_energy(E)
-    
+    e_norm = norm_energy(e)
+
     # Train parameters of gaussian mixture models
-    w, m, sigma = gmm_train(E_norm, gauss_pdf, n_realignment=10)
-    
+    w, m, sigma = gmm_train(e_norm, gauss_pdf, n_realignment=n_realignment)
+
     # Estimate a posterior probability that frame isn't speech
-    g0 = eval_frame_post_prob(E_norm, gauss_pdf, w, m, sigma)
-    
+    g0 = eval_frame_post_prob(e_norm, gauss_pdf, w, m, sigma)
+
     # Compute real VAD's markup
-    vad_frame_markup_real = (g0 < vad_thr).astype('float32')  # frame VAD's markup
-
-    vad_markup_real = np.zeros(len(signal)).astype('float32') # sample VAD's markup
+    vad_frame_markup_real = (g0 < vad_thr).astype('float32')
+    # Compute sample VAD's markup
+    vad_markup_real = np.zeros(len(signal)).astype('float32')
     for idx in range(len(vad_frame_markup_real)):
-        vad_markup_real[idx*shift:shift+idx*shift] = vad_frame_markup_real[idx]
+        vad_markup_real[idx * shift:shift + idx * shift] = vad_frame_markup_real[idx]
 
-    vad_markup_real[len(vad_frame_markup_real)*shift - len(signal):] = vad_frame_markup_real[-1]
-    
+    vad_markup_real[len(vad_frame_markup_real) * shift - len(signal):] = vad_frame_markup_real[-1]
+
     # Morphology Filters
-    vad_markup_real = closing(vad_markup_real, np.ones(mask_size_morph_filt)) # close filter
-    vad_markup_real = opening(vad_markup_real, np.ones(mask_size_morph_filt)) # open filter
-    
+    # close filter
+    vad_markup_real = closing(vad_markup_real, np.ones(mask_size_morph_filt))
+    # open filter
+    vad_markup_real = opening(vad_markup_real, np.ones(mask_size_morph_filt))
+
     return vad_markup_real
 
-def reverb(signal, impulse_response):
-    # Function to create reverberation effect
-    
+
+def reverb(signal: np.ndarray, impulse_response: np.ndarray) -> np.ndarray:
+    """
+    Function to create reverberation effect
+
+    :param signal: raw input signal
+    :param impulse_response: impulse response for augmentation
+    :return: signal_reverb: reverberated signal
+
+    TODO: Here you need to reverberate input raw signal with target impulse response
+
+    """
+
     signal_reverb = np.zeros(len(signal)).astype('float32')
-    
+
     ###########################################################
     # Here is your code
-    
+
     ###########################################################
-    
+
     return signal_reverb
 
-def awgn(signal, sigma_noise):
-    # Function to add white gaussian noise to signal
-    
+
+def awgn(signal: np.ndarray, sigma_noise: float) -> np.ndarray:
+    """
+    Function to add white gaussian noise to signal
+
+    :param signal: raw input signal
+    :param sigma_noise: sigma of noise to add
+    :return: signal_noise: noised signal
+
+    TODO: Here you need to make additive white gaussian noise augmentation
+    """
     signal_noise = np.zeros(len(signal)).astype('float32')
-    
+
     ###########################################################
     # Here is your code
-    
+
     ###########################################################
-    
+
     return signal_noise
+
+
